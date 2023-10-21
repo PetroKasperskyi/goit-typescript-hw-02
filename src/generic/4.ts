c05865118d44e1bc89249f4d1e13cd3913c02591
PetroKasperskyi <kaspersky28061993@gmail.com>
2023-10-21 10:06:07 +0300

    done

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,17 +1,18 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
+interface T {
+  title: string;
+};
 
-class Component {
-  constructor (public props:T) {
+class Component<T> {
+  constructor (public props:T) {}
+};
 
-  }
-}
-
-class Page extends Component {
+class Page extends Component<T> {
   pageInfo () {
     console.log(this.props.title);
   }
-}
+};
 
 export {};
